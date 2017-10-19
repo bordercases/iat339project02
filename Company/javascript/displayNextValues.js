07312c863675504eeadeb6f4a53e821e93b40c52
Border Cases <kenneth.bruskiewicz@gmail.com>
2017-10-19 16:52:49 -0700

    Sommelier expands to fill the screen at all times

--- Company/javascript/displayNextValues.js
+++ Company/javascript/displayNextValues.js
@@ -4,24 +4,36 @@
 
 // select elements determined by slider
 var sliderOptions = document.getElementsByClassName('slider-option');
+var sliderContents = document.getElementsByClassName('slider-content');
 // initialize all as hidden
 for (var i = 0; i < sliderOptions.length; i++) {
   var sliderOption = sliderOptions[i];
   sliderOption.style.display = "none"
+}
+for (var i = 0; i < sliderContents.length; i++) {
+  var sliderContent = sliderContents[i];
+  sliderContent.style.display = "none"
 }
 
 // set the current value as not hidden
 var sliderInput = document.getElementsByTagName('input');
 var firstValue = sliderOptions.length - (parseInt(sliderInput[0].value))
 sliderOptions[firstValue].style.display = "inline"
+sliderContents[firstValue].style.display = "inline"
 
 document.addEventListener("input", function(event) {
   for (var i = 0; i < sliderOptions.length; i++) {
     var sliderOption = sliderOptions[i];
+    var sliderContent = sliderContents[i];
+
     if (i != (sliderOptions.length - parseInt(event.target.value))) {
       sliderOption.style.display = "none"
+      sliderContent.style.display = "none"
+
     } else {
       sliderOption.style.display = "inline"
+      sliderContent.style.display = "inline"
+
     }
   }
 });

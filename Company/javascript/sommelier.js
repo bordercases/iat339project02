956bbba688bbec590a985d746562dd80e87cbaec
bordercases <kenneth.bruskiewicz@gmail.com>
2017-10-24 11:10:54 -0700

    WIP: Getting images to work with Sommelier

--- Company/javascript/sommelier.js
+++ Company/javascript/sommelier.js
@@ -1,3 +1,4 @@
+'use strict';
 // Keeps track of Sommelier state and sets it between pages
 
 if (sessionStorage.getItem("topping") === null ) {
@@ -13,3 +14,31 @@ var topping = sessionStorage.getItem("topping");
 // set sommelier image to the current variables
 var sommImg = document.getElementById('somm-img');
 sommImg.src = "img/png/coffee/"+richness+"-"+topping+".png";
+
+var inputRadios = document.querySelectorAll("input[type=radio]");
+for (var i = 0; i < inputRadios.length; i++) {
+  inputRadios[i].addEventListener(
+    "change",
+    function (e) {
+      var name = e.target.name;
+      var value = e.target.value;
+      console.log(name+" "+value)
+      refreshImage({ name : value });
+    }
+  );
+}
+var refreshImage = function (data) {
+  if (data["richness"]) {
+    sessionStorage.setItem("richness", data["richness"]);
+  }
+  if (data["topping"]) {
+    sessionStorage.setItem("topping", data["topping"]);
+  }
+  // if (data["flavor"]) {
+  //   sessionStorage.setItem("flavor", data["flavor"]);
+  // }
+  var sommImg = document.getElementById('somm-img');
+  var richness = sessionStorage.getItem("richness");
+  var topping = sessionStorage.getItem("topping");
+  sommImg.src = "img/png/coffee/"+richness+"-"+topping+".png";
+}
--- Company/javascript/wrap.js
+++ Company/javascript/wrap.js
@@ -14,22 +14,32 @@ window.addEventListener(
     if (width <= emToPix(emBarrier)) {
       // hide sommelier controls
       var sommControls = document.querySelectorAll(".somm-control");
-      var sommNav = document.querySelectorAll(".somm-nav");
+      var switchOptions = document.querySelector("#switchOptions");
       for (var i = 0; i < sommControls.length; i++) {
         sommControls[i].style.display = "none";
-        sommNav[i].style.display = "none"
       }
       // unhide active one
       sommControls[defaultSommControl].style.display = "block";
-      sommNav[i].style.display = "block";
-      // switch to the vertical version
-    } else if (width > emToPix(emBarrier)) {
+      switchOptions.style.display = "block";
+    }
+    if (width > emToPix(emBarrier)) {
       // unhide sommelier controls
       var sommControls = document.querySelectorAll(".somm-control");
+      var switchOptions = document.querySelector("#switchOptions");
       for (var i = 0; i < sommControls.length; i++) {
         sommControls[i].style.display = "block";
-        sommNav[i].style.display = "block"
       }
+      switchOptions.style.display = "none";
     }
+  }
+);
+
+document.querySelector("#switchOptions").addEventListener(
+  "click",
+  function(e) {
+      var sommControls = document.querySelectorAll(".somm-control");
+      sommControls[defaultSommControl].style.display = "none";
+      defaultSommControl = (defaultSommControl + 1) % 3;
+      sommControls[defaultSommControl].style.display = "block";
   }
 );

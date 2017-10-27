7be303774d102116e0c47c149350f15fcf8604cf
Unknown <nancy_chen107@hotmail.com>
2017-10-27 02:21:39 -0700

    Commented Pages
    Added a fade in animation to body, added  paddings in citation page, thinned nav header padding

--- Company/javascript/hamburger.js
+++ Company/javascript/hamburger.js
@@ -2,17 +2,17 @@
 
 // var nameofVariable= object.property
 window.innerWidth;
-var windowWidth= window.innerWidth;
-console.log("window width= "+ windowWidth+ "px");
+var windowWidth = window.innerWidth;
+console.log("window width= " + windowWidth + "px");
 
 // takes in a css selector, willonly find the first one (querySelectorAll finds all)
-var navDefault=document.querySelector("#nav-default");
-var navToggle=document.querySelector("#nav-toggle");
-var navItems=document.querySelector("#nav-items");
-var navFirstItem=document.querySelector("#nav-items a");
+var navDefault = document.querySelector("#nav-default");
+var navToggle = document.querySelector("#nav-toggle");
+var navItems = document.querySelector("#nav-items");
+var navFirstItem = document.querySelector("#nav-items a");
 
 
-if (windowWidth<500) {
+if (windowWidth < 500) {
   console.log("Window width is less than 500px, collapsing menu");
 
   // classList expllicitly specify it s aast
@@ -22,50 +22,50 @@ if (windowWidth<500) {
 }
 
 //acccessibility tool support
-navToggle.setAttribute("aria-hidden","false");
-navItems.setAttribute("aria-hidden","true");
-navItems.setAttribute("aria-labelledby","nav-toggle");
+navToggle.setAttribute("aria-hidden", "false");
+navItems.setAttribute("aria-hidden", "true");
+navItems.setAttribute("aria-labelledby", "nav-toggle");
 
 //when hamburger is clicked, show the nav list
-navToggle.addEventListener("click",function(){
+navToggle.addEventListener("click", function() {
   console.log("navToggle has been clicked");
 
   if (navItems.classList.contains("hidden")) {
     console.log("navitems hidden, showing now");
 
-    navItems.classList.remove("hidden");//showing items
+    navItems.classList.remove("hidden"); //showing items
 
     //acccessibility
-    navItems.setAttribute("aria-hidden","false ");
-    navToggle.setAttribute("aria-expanded","true ");
+    navItems.setAttribute("aria-hidden", "false ");
+    navToggle.setAttribute("aria-expanded", "true ");
 
     //jumps to first menu item right away(a variable here);
     navFirstItem.focus();
 
-  }else {
+  } else {
     navItems.classList.add("hidden");
-    navItems.setAttribute("aria-hidden","true ");
-    navToggle.setAttribute("aria-expanded","false ");
+    navItems.setAttribute("aria-hidden", "true ");
+    navToggle.setAttribute("aria-expanded", "false ");
   }
 });
 
 //when window is resized, show mobile nav
-window.addEventListener("resize",function(){
-  var width= document.documentElement.clientWidth;
+window.addEventListener("resize", function() {
+  var width = document.documentElement.clientWidth;
   // console.log("window width:"+ width);
 
-  if (width<500) {
+  if (width < 500) {
     console.log("mobile nav hidden, showing it now");
 
     //hide default nav, show mobile nav
     navDefault.classList.add("hidden");
-    navToggle.classList.remove("hidden");//showing mobile nav
+    navToggle.classList.remove("hidden"); //showing mobile nav
     navItems.classList.add("hidden");
 
     //jumps to first menu item right away(a variable here);
     navFirstItem.focus();
 
-  }else {
+  } else {
     navItems.classList.add("hidden");
     navToggle.classList.add("hidden");
     navDefault.classList.remove("hidden");
